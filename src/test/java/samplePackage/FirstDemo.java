8e89877be33a4cdd577a8ef4ded9c8e712e1939a
Hua Yang <shinasaurus@gmail.com>
2022-09-13 19:36:31 -0700

    third commit

--- src/test/java/samplePackage/FirstDemo.java
+++ src/test/java/samplePackage/FirstDemo.java
@@ -4,6 +4,7 @@ public class FirstDemo {
 
 	public static void main(String[] args) {
 		System.out.println("First edit after creation");
+		System.out.println("7:31 pm edit after second commit ");
 	}
 
 }
